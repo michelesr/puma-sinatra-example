93f5004ec2bb03dd85e78ecf5ab90fa810ff38dc
Michele Sorcinelli <michele.sorcinelli@citizensadvice.org.uk>
2017-04-13 16:44:09 +0100

    Add some fancy methods :-)

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
 
+SLEEP_SECONDS = ENV.fetch('SLEEP_SECONDS') {1}.to_f
+
 class Application < Sinatra::Base
   get '/' do
-    "Hello World!"
+    'Hello World!'
+  end
+
+  get '/random' do
+    sleep_seconds = rand * SLEEP_SECONDS
+    sleep(sleep_seconds)
+    sleep_seconds.to_s
+  end
+
+  get '/fixed' do
+    sleep(SLEEP_SECONDS)
+    SLEEP_SECONDS.to_s
   end
 end
